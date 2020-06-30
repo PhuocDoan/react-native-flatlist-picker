9d0a6167adb6a0b44d480b8e48d67d25410afc42
PhuocDoan <doanhuuphuo98@gmail.com>
2020-06-30 14:02:57 +0700

    Release version 1.0.0

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Phuoc Doan on 06/30/2020.
+ */
+
+import FlatListPicker from './src/FlatListPicker';
+export default FlatListPicker;
+module.exports = FlatListPicker;
---
+++ src/FlatListPicker.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Phuoc Doan on 06/30/2020.
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import FlatlistDropdown from './FlatlistDropdown';
+
+
+class FlatListPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: '0',
+      showDrop: false
+    };
+  }
+
+  showPickerList = () => {
+    this.ModalDropdown.show()
+  }
+
+  hidePickerList = () => {
+    this.ModalDropdown.hide()
+  }
+
+  render() {
+    const { data, animated, renderItem, disableTouchContainer, renderDropdownIcon, containerStyle, pickerStyle, dropdownStyle, dropdownTextStyle, pickedTextStyle } = this.props;
+    return (
+      <TouchableOpacity
+        style={[styles.container, { ...containerStyle }]}
+        onPress={() => this.ModalDropdown.show()}
+        disabled={disableTouchContainer || false}
+      >
+        <FlatlistDropdown
+          {...this.props}
+          ref={ref => { this.ModalDropdown = ref }}
+          options={data}
+          animationType={animated}
+          style={pickerStyle && pickerStyle}
+          dropdownStyle={dropdownStyle && dropdownStyle}
+          dropdownTextStyle={dropdownTextStyle && dropdownTextStyle}
+          textStyle={pickedTextStyle}
+          renderRow={renderItem && renderItem}
+        />
+        {renderDropdownIcon
+          ? renderDropdownIcon()
+          : (<AntDesign name="caretdown" size={10} style={{ padding: 15 }} />)
+
+        }
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default FlatListPicker;
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: '#CECECE',
+    width: 200,
+    height: 50,
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 5,
+    flexDirection: 'row',
+  }
+});
